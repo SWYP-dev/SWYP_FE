98c3d9903fb03ac0553b3c2837ebd7afb73cbe32
xodudgks <legoherohty@gmail.com>
2026-07-23 20:57:30 +0900

    fix: S3 썸네일 도메인 next/image remotePatterns 등록 및 ScrapCard 썸네일 null 방어

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,14 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
-const nextConfig: NextConfig = {/* config options here */};
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'swyp-prod-177989594187-ap-southeast-2-an.s3.ap-southeast-2.amazonaws.com',
+      },
+    ],
+  },
+};
 
 export default nextConfig;
