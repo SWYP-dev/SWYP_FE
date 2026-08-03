c5f80625d47f8d9417a67ccc51d3dad3d7e81a9d
xodudgks <legoherohty@gmail.com>
2026-08-04 02:32:06 +0900

    design: URL 카테고리 메뉴 아이템 간 gap 2px로 수정

--- src/features/kanban/components/CardDetailDrawer.tsx
+++ src/features/kanban/components/CardDetailDrawer.tsx
@@ -364,7 +364,7 @@ export function CardDetailDrawer({
                         <ChevronDownIcon />
                       </button>
                       {showCategoryDropdown && (
-                        <div className="absolute bottom-[calc(100%+4px)] left-0 z-10 w-[108px] overflow-hidden rounded-xl border border-line-secondary bg-base-white p-1 shadow-spread-small">
+                        <div className="absolute bottom-[calc(100%+4px)] left-0 z-10 flex w-[108px] flex-col gap-1 overflow-hidden rounded-xl border border-line-secondary bg-base-white p-1 shadow-spread-small">
                           {URL_CATEGORIES.map((cat) => (
                             <button
                               key={cat.value}
