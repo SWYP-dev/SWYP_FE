96e99f8f7762598a74da5b26b82ed0e9b89b39a3
xodudgks <legoherohty@gmail.com>
2026-08-07 05:52:40 +0900

    fix: LandingAuthRedirect children prop 누락으로 인한 빌드 타입 에러 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,18 +12,19 @@ export default function Landing() {
   return (
     // landing-root: globals.css의 html:has(.landing-root) 부드러운 스크롤 스코프용
     <div className="landing-root min-h-screen bg-white">
-      {/* 로그인 상태면 /jobs로 이동 (렌더링 결과 없음) */}
-      <LandingAuthRedirect />
-      <Navbar />
-      <main>
-        <Hero />
-        <Problem />
-        <Solution />
-        <FeatureGrid />
-        <Audience />
-        <CtaSection />
-      </main>
-      <Footer />
+      {/* 로그인 상태(또는 hydration 완료 전)면 콘텐츠를 그리지 않고 /jobs로 이동시킨다 */}
+      <LandingAuthRedirect>
+        <Navbar />
+        <main>
+          <Hero />
+          <Problem />
+          <Solution />
+          <FeatureGrid />
+          <Audience />
+          <CtaSection />
+        </main>
+        <Footer />
+      </LandingAuthRedirect>
     </div>
   );
 }
